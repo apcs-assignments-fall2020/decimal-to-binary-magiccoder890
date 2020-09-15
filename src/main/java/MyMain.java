962b662ae60fee3c95cd95a770c5210a3bdd4f00
magiccoder890 <23fzhu@athenian.org>
2020-09-15 22:13:53 +0000

    This is Bert's new homework

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -2,13 +2,29 @@ import java.util.Scanner;
 
 public class MyMain {
     
-    public static int binaryToDecimal(String binary) { 
-        return -1;
-        // REPLACE WITH YOUR CODE HERE
+    public static int binaryToDecimal(String binary) {
+        int num = Integer.parseInt(binary);
+        int modulo = 0;
+        int answer = 0;
+        int adding = 0;
+        for (int i = 0; i < binary.length(); i++){
+            modulo = num % 10;
+            adding = (int) Math.pow(2,i);
+            if (modulo == 1) {
+                answer += adding;
+            }
+            else {
+                answer += 0;
+            }
+            num /= 10;
+        }
+
+        return answer;
     }
-    
-    
-    public static void main(String[] args) {
-        // YOUR CODE HERE
+    public static void main(String[] arg) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Type in a number in binary format: ");
+        String binary = scanner.next();
+    System.out.print(binaryToDecimal(binary));
     }
 }
